c73ed0d20d94d74c0cd54b3f7156a98d4e052218
ddhp <jessechen.jcc@gmail.com>
2019-08-15 19:41:26 +0900

    tmp

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,39 @@
+const ExtractCSSChunksPlugin = require('extract-css-chunks-webpack-plugin');
+
 module.exports = {
     mode: 'development',
     entry: {
         main: './index.js',
     },
+    plugins: [
+        new ExtractCSSChunksPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // all options are optional
+            filename: '[name].css',
+            chunkFilename: '[id].css',
+            // ignoreOrder: false, // Enable to remove warnings about conflicting order
+        }),
+    ],
     module: {
         rules: [
             {
                 test: /\.css$/i,
-                use: ['style-loader', {
-                    loader: 'css-loader',
-                    options: {
-                        modules: true,
+                use: [
+                    {
+                        loader: ExtractCSSChunksPlugin.loader,
+                        options: {
+                            // you can specify a publicPath here
+                            // by default it uses publicPath in webpackOptions.output
+                            publicPath: '../',
+                            hot: process.env.NODE_ENV !== 'production',
+                        },
+                    }, {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                        },
                     },
-                }],
+                ],
             },
         ],
     },
